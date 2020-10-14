a26b7b5f900b8c70e8a2c85825e5d5ac8c32b87c
rocky fierro <stray17@icloud.com>
2020-10-13 18:34:38 -0700

    Updates

---
+++ precache-manifest.a9ac96c265a3eecf3345894912056d17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2fcb1bf08c1d3a871727d0fd7006fde",
+    "url": "/learn/index.html"
+  },
+  {
+    "revision": "d9ddc69302443543399e",
+    "url": "/learn/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53ef8537b361d9f9afeb",
+    "url": "/learn/static/js/2.b2f6b197.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn/static/js/2.b2f6b197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ddc69302443543399e",
+    "url": "/learn/static/js/main.33ba69e1.chunk.js"
+  },
+  {
+    "revision": "42deb66135edfba6bd32",
+    "url": "/learn/static/js/runtime-main.29724945.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn/static/media/logo.5d5d9eef.svg"
+  }
+]);
